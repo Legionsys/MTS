6af4c675729f1befd73af3970eba1f8cb980ca41
Legion <mailewok@gmail.com>
2023-03-23 21:12:21 +0800

    incremental 2303

--- inc/job-init.php
+++ inc/job-init.php
@@ -6,14 +6,19 @@ if (isset($_POST['jbn'])) {
 if (isset($_POST['indi'])) {
     $indi = trim($_POST['indi']);
 };
-
+$emparray = array();
 require_once 'dbh.inc.php';
 
 if ($indi == "job") {
     $sql = "SELECT clientName AS client, jobRef, contd AS cliContact, contPh AS cliContPh, contEm AS cliContEm, contEm2 AS cliContEm2, jobDate AS puDate, jobFin AS doDate, jobComp AS ac_cb, jobInv AS inv_c, invNum as InvNum, cnam, cadd1, cadd2, cadd3, cst, cpc, cCtc, cPh, cEm, cCtc2 AS j_r_ct2, cPh2 AS j_r_ph2, cEm2 AS j_r_em2, dnam, dadd1, dadd2, dadd3, dst, dpc, dCtc, dPh, dEm, dCtc2 as j_e_ct2, dPh2 AS j_e_ph2, dEm2 AS j_e_em2 FROM jobList LEFT JOIN clientList on jobList.clientId=clientList.clientId WHERE jobID = ? limit 1;";
 } elseif ($indi == "sup") {
     $sql = "SELECT * FROM jobSup WHERE jobID = ? order by jsID desc;";
+} elseif ($indi == "not") {
+    $sql = "SELECT * FROM jobNote WHERE jobID = ? and jnDie is null order by jnOrd;";
+} elseif ($indi == "con") {
+    $sql = "SELECT a.*,b.titm,b.twgt,b.tcub FROM conNotes a LEFT JOIN (select cnID,sum(ifnull(noItem,0)) as titm,sum(ifnull(itWgt,0)) as twgt,sum(ifnull((itLen*itWid*itHei*itQty)/1000000,0)) as tcub from conDets WHERE frtDie is Null group by cnID) b on a.cnID = b.cnID WHERE a.jobID = ? order by cnID;";
 }
+
 
 
 //$sql = "SELECT * FROM conNotes where cnID=?";
@@ -26,11 +31,14 @@ mysqli_stmt_bind_param($stmt, "i", $job);
 mysqli_stmt_execute($stmt);
 
 $resultData = mysqli_stmt_get_result($stmt);
-if ($row = mysqli_fetch_assoc($resultData)) {
-    echo json_encode($row);
-} else {
+if (mysqli_num_rows($resultData) > 0){
+    while ($row = mysqli_fetch_assoc($resultData)) {
+        $emparray[] = $row;
+    }
+    echo json_encode($emparray);
+ } else {
     $result = false;
-    echo "<script>console.log('error on row');</script>";     
+    echo "<script>console.log('error on row within job ".$job." for ind ".$indi."');</script>";     
 }
 
 mysqli_stmt_close($stmt);
--- js/jdet2.js
+++ js/jdet2.js
@@ -6,6 +6,41 @@ var frt = [];
 var jbn;
 var upd;
 
+number_format = function (number, decimals, dec_point, thousands_sep) {
+    number = Number(number).toFixed(decimals);
+  
+    var nstr = number.toString();
+    nstr += '';
+    x = nstr.split('.');
+    x1 = x[0];
+    x2 = x.length > 1 ? dec_point + x[1] : '';
+    var rgx = /(\d+)(\d{3})/;
+  
+    while (rgx.test(x1))
+        x1 = x1.replace(rgx, '$1' + thousands_sep + '$2');
+  
+    return x1 + x2;
+  };
+
+  function namt_tot(){
+    var tots = parseFloat(0);
+  $('#notebody > .tr > .namt').each(function() {
+    if ($.isNumeric($(this).html())) {
+      tots = tots + parseFloat($(this).html().replace(/\,/g,""));
+      } else {
+        if ($.isNumeric($(this).html().replace(/\,/g,""))) {
+            tots = tots + parseFloat($(this).html().replace(/\,/g,""));
+        }
+      }
+  })
+  
+  if (tots != parseFloat(0)) {
+    $('#ahead').html(number_format(tots,2,'.',','));
+  } else {
+    $('#ahead').html("Amount");
+  }
+  };
+
 var getUrlParameter = function getUrlParameter(sParam) {
     var sPageURL = window.location.search.substring(1),
         sURLVariables = sPageURL.split('&'),
@@ -38,6 +73,8 @@ function firstpop(){
     upd = "y";
     jbdu();
     jbsu();
+    jbnot();
+    jbcon();
 };
 function jsupd(){
     $(jdets).each(function (i, val) {
@@ -54,18 +91,66 @@ function jsupd(){
 };
 function jsupupd() {
     $(sups).each(function (i, val) {
+        var txt = "";
+        txt = '<div class="supln" data-id="' + val.jsID + '">'
+        if (val.jsName === null || val.jsName === undefined) {
+            txt = txt + '<div contenteditable="true" data-col="jsName" class="supSu lsup"></div>';
+        } else {
+            txt = txt + '<div contenteditable="true" data-col="jsName" class="supSu lsup">' + val.jsName + '</div>';
+        }
+        if (val.jsType === null || val.jsType === undefined) {
+            txt = txt + '<div contenteditable="true" data-col="jsType" class="supTy lsup"></div>';
+        } else {
+            txt = txt + '<div contenteditable="true" data-col="jsType" class="supTy lsup">' + val.jsType + '</div>';
+        }
 
-        $.each(val, function (k, v) {
-            if (k == "ac_cb" || k == "inv_c" ) {
-                if (v !== null){
-                    $('#' + k).prop('checked', true);
-                }
-            } else {
-                $("#" + k).val(v).attr('value',v);
-            }
-            });
+        if (val.jsDesc === null || val.jsDesc === undefined) {
+            txt = txt + '<div contenteditable="true" data-col="jsDesc" class="supDe lsup"></div>';
+        } else {
+            txt = txt + '<div contenteditable="true" data-col="jsDesc" class="supDe lsup">' + val.jsDesc + '</div>';
+        }
 
+        if (val.jsEst === null || val.jsEst === undefined) {
+            txt = txt + '<div contenteditable="true" data-col="jsEst" class="supEc lsup"></div>';
+        } else {
+            txt = txt + '<div contenteditable="true" data-col="jsEst" class="supEc lsup">' + val.jsEst + '</div>';
+        }
+        if (val.jsInvRec === null || val.jsInvRec === undefined) {
+            txt = txt + '<div contenteditable="true" data-col="jsInvRec" class="supIr lsup"></div>';
+        } else {
+            txt = txt + '<div contenteditable="true" data-col="jsInvRec" class="supIr lsup">' + val.jsInvRec + '</div>';
+        }
+        if (val.jsNotes === null || val.jsNotes === undefined) {
+            txt = txt + '<div contenteditable="true" data-col="jsNotes" class="supNo lsup"></div>';
+        } else {
+            txt = txt + '<div contenteditable="true" data-col="jsNotes" class="supNo lsup">' + val.jsNotes + '</div>';
+        }
+        txt = txt + '<div class="suprm td" data-id="' + val.jsID + '">Remove Supplier</div></div>';
+        $("#supbody").append(txt);
+        
     });
+};
+function jnotupd() {
+    $(notes).each(function (i, val) {
+        var txt = "";
+        txt = '<div class="tr" data-id="' + val.jnID + '"><div contenteditable="true" data-col="jnNote" class="ncol td">'
+        if (val.jnNote === null || val.jnNote === undefined) {
+            txt = txt + '</div>';
+        } else {
+            txt = txt + val.jnNote + '</div>';
+        }
+        txt = txt + '<div contenteditable="true" data-col="jnAmt" class="namt td">';
+        if (val.jnAmt === null || val.jnAmt === undefined) {
+            txt = txt + '</div>';
+        } else {
+            txt = txt + number_format(val.jnAmt,2,'.',',') + '</div>';
+        }
+
+        txt = txt + '<div class="ntra td"><div class="cmd_img" data-id="' + val.jnID + '"><img class="ntrash" class="nbut" alt="Delete Note" src="/img/trash.svg"></div></div></div>';
+        $("#notebody").append(txt);
+        
+    });
+    namt_tot();
 };
 function jbdu(){
     var ind = "job";
@@ -73,7 +158,6 @@ function jbdu(){
     { jbn: jbn , indi: ind }, 
     function (data, status) {
         jdets = $.parseJSON(data);
-        console.log("Success");
         if (upd === "y") {
             jsupd();
         }
@@ -89,7 +173,6 @@ function jbsu(){
     { jbn: jbn , indi: ind }, 
     function (data, status) {
         sups = $.parseJSON(data);
-        console.log("Success");
         if (upd === "y") {
             jsupupd();
         }
@@ -98,7 +181,39 @@ function jbsu(){
             console.log("Error: " + response.responseText);
             console.log(response);
         });    
-}
+};
+function jbnot(){
+    var ind = "not";
+    $.post("/inc/job-init.php", 
+    { jbn: jbn , indi: ind }, 
+    function (data, status) {
+        notes = $.parseJSON(data);
+        if (upd === "y") {
+            jnotupd();
+        }
+    }).fail(function (response) {
+        console.log("Failure Supplier - " + jbn);
+        console.log("Error: " + response.responseText);
+        console.log(response);
+    });   
+};
+function jbcon(){
+    var ind = "con";
+    $.post("/inc/job-init.php", 
+    { jbn: jbn , indi: ind }, 
+    function (data, status) {
+        console.log(data);
+        cnot = $.parseJSON(data);
+        console.log(cnot);
+        if (upd === "y") {
+            //jnotupd();
+        }
+    }).fail(function (response) {
+        console.log("Failure Supplier - " + jbn);
+        console.log("Error: " + response.responseText);
+        console.log(response);
+    });   
+};
 $(document).ready(function () {
     jbn = getSearchParams("job_no");
     firstpop();
