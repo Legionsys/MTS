475049f3d9806e57fc93315243dd80bc73fb8c50
BigT <109352897+Legionsys@users.noreply.github.com>
2023-12-18 20:16:21 +0800

    Update dbh.inc.php

--- inc/dbh.inc.php
+++ inc/dbh.inc.php
@@ -1,15 +1,15 @@
 <?php
 
 
-/*A2*/
+//A2
 $serverName = "sg1-lr2.a2hosting.com";
 $dBUsername = "aden_webusr";
 $dBPassword = "Webusr_password";
 $dBName = "aden_MOORTSP";
 
 
-/*local
-$serverName = "localhost";
+//local
+/*$serverName = "localhost";
 $dBUsername = "root";
 $dBPassword = "";
 $dBName = "aden_MOORTSP";*/
