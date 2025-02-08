cf05f053bb58e48bca738c6cbb2210cede6a4673
Legion <mailewok@gmail.com>
2025-02-08 20:16:04 +0800

    PHP 8.3

--- inc/job-summ-upd.php
+++ inc/job-summ-upd.php
@@ -17,19 +17,18 @@ if (isset($_POST['inv'])) {
     $inv = trim($_POST['inv']);
 };
 
-$tags = isset($_POST['tags']) ? $_POST['tags'] : null;
-if ($tags = 'null') {
-    $tags == null;
-}
+if (isset($_POST['tags'])) {
+    $tags = trim($_POST['tags']);
+};
 
 define("FS_ROOT", realpath(dirname(__FILE__)));
-require_once FS_ROOT.'/dbh.inc.php';
+require_once FS_ROOT . '/dbh.inc.php';
 
-require FS_ROOT.'/jobfunct.inc.php';
+require FS_ROOT . '/jobfunct.inc.php';
 
 
 
-if ($client == 'null' || $client == 'false' || $client == 'All'){
+if ($client == 'null' || $client == 'false' || $client == 'All') {
     $client = '';
 };
 
@@ -41,56 +40,54 @@ if (strlen($client) > 0) {
     if (is_numeric($client) == 1) {
         $clientId = $client;
     } else {
-        $clientId = cliIdexist($conn,$client);
+        $clientId = cliIdexist($conn, $client);
     }
 }
 
-
 $sql = "SELECT * FROM jobList LEFT JOIN clientList on jobList.clientId = clientList.clientId WHERE ";
 
-if (strlen($clientId) > 0){
-    $sql = $sql."jobList.clientId = $clientId AND ";
+if (strlen($clientId) > 0) {
+    $sql = $sql . "jobList.clientId = $clientId AND ";
 };
-if (strlen($wild) > 0){
-    $sql = $sql."concat(ifnull(jobList.jobID,'') ,ifnull(clientList.clientName,'') ,ifnull(jobList.contd,'') ,ifnull(jobList.contPh,'') ,ifnull(jobList.contEm,'') ,ifnull(jobList.contEm2,'') ,ifnull(jobList.jobRef,'') ,ifnull(jobList.invNum,'') ,ifnull(jobList.cnam,'') ,ifnull(jobList.cadd1,'') ,ifnull(jobList.cadd2,'') ,ifnull(jobList.cadd3,'') ,ifnull(jobList.cst,'') ,ifnull(jobList.cCtc,'') ,ifnull(jobList.cEm,'') ,ifnull(jobList.cCtc2,'') ,ifnull(jobList.cEm2,'') ,ifnull(jobList.dnam,'') ,ifnull(jobList.dadd1,'') ,ifnull(jobList.dadd2,'') ,ifnull(jobList.dadd3,'') ,ifnull(jobList.dst,'') ,ifnull(jobList.dCtc,'') ,ifnull(jobList.dEm,'') ,ifnull(jobList.dCtc2,'') ,ifnull(jobList.dEm2,'')) like '%$wild%' AND ";
+if (strlen($wild) > 0) {
+    $sql = $sql . "(concat(ifnull(jobList.jobID,'') ,ifnull(clientList.clientName,'') ,ifnull(jobList.contd,'') ,ifnull(jobList.contPh,'') ,ifnull(jobList.contEm,'') ,ifnull(jobList.contEm2,'') ,ifnull(jobList.jobRef,'') ,ifnull(jobList.invNum,'') ,ifnull(jobList.cnam,'') ,ifnull(jobList.cadd1,'') ,ifnull(jobList.cadd2,'') ,ifnull(jobList.cadd3,'') ,ifnull(jobList.cst,'') ,ifnull(jobList.cCtc,'') ,ifnull(jobList.cEm,'') ,ifnull(jobList.cCtc2,'') ,ifnull(jobList.cEm2,'') ,ifnull(jobList.dnam,'') ,ifnull(jobList.dadd1,'') ,ifnull(jobList.dadd2,'') ,ifnull(jobList.dadd3,'') ,ifnull(jobList.dst,'') ,ifnull(jobList.dCtc,'') ,ifnull(jobList.dEm,'') ,ifnull(jobList.dCtc2,'') ,ifnull(jobList.dEm2,'')) like '%$wild%' ";
+    $sql .= " OR EXISTS (
+        SELECT 1 FROM conNotes 
+        WHERE conNotes.jobID = jobList.jobID 
+        AND conNotes.cnNum LIKE '%$wild%'
+    )) AND ";
 };
 
-if ($job == "Act"){
-    $sql = $sql."jobComp IS NULL AND ";
+if ($job == "Act") {
+    $sql = $sql . "jobComp IS NULL AND ";
 };
-if ($job == "Com"){
-    $sql = $sql."jobComp IS NOT NULL AND ";
-}; 
-
-
-if ($inv == "Pend"){
-    $sql = $sql."jobComp IS NULL AND ";
-} 
-if ($inv == "Com"){
-    $sql = $sql."jobComp IS NOT NULL AND ";
+if ($job == "Com") {
+    $sql = $sql . "jobComp IS NOT NULL AND ";
+};
+if ($inv == "Pend") {
+    $sql = $sql . "jobInv IS NULL AND ";
+}
+if ($inv == "Com") {
+    $sql = $sql . "jobInv IS NOT NULL AND ";
 };
 
 if ($tags && $tags != null && $tags != 'null') {
     // Assuming you join `jobTags` table with the job table, you can filter jobs based on the selected tags.
     $tagsArray = explode(',', $tags); // Convert tags string to array
     // Example: Modify your SQL query to add a WHERE condition that matches job tags
-    //echo "<script>console.log('tagsarray - ".$tagsArray."');</script>";
-    //$sql .= "jobID IN (SELECT job FROM jobTags WHERE removed IS null AND tag IN (" . implode(',', array_map('intval', $tagsArray)) . "))";
-$sql .= "jobID IN (SELECT job FROM jobTags WHERE removed IS null AND tag IN (" . implode(',', array_map(function($tag) {
-    return is_numeric($tag) ? (int) $tag : '"' . addslashes($tag) . '"';
+    $sql .= "jobID IN (SELECT job FROM jobTags WHERE removed IS null AND tag IN (" . implode(',', array_map(function ($tag) {
+        return is_numeric($tag) ? (int) $tag : '"' . addslashes($tag) . '"';
     }, $tagsArray)) . "))";
 };
 
-
-
-if (substr($sql,-5) == " AND "){
-    $sql = substr($sql,0,strlen($sql)-4);
+if (substr($sql, -5) == " AND ") {
+    $sql = substr($sql, 0, strlen($sql) - 4);
 };
-if (substr($sql,-5) == "HERE "){
-    $sql = substr($sql,0,strlen($sql)-6);
+if (substr($sql, -5) == "HERE ") {
+    $sql = substr($sql, 0, strlen($sql) - 6);
 };
 
-$sql = $sql."order by jobDate is Null, jobDate ASC;";/*
+$sql = $sql . "order by jobDate is Null, jobDate ASC;";/*
 if (strlen($clientId) > 0 and strlen($wild) > 0) {
     $sql = "SELECT * FROM jobList LEFT JOIN clientList on jobList.clientId = clientList.clientId WHERE jobRef like '%$wild%' AND clientId = $clientId order by jobID;";
 } elseif (strlen($clientId) > 0 and strlen($wild) == 0) {
@@ -102,35 +99,36 @@ if (strlen($clientId) > 0 and strlen($wild) > 0) {
 }    */
 
 
-//echo '<script>console.log("'.$sql.'");</script>';
-$resultData = mysqli_query($conn,$sql);
-if (mysqli_num_rows($resultData) > 0){
+//echo '<script>console.log("SQL Query: ' . addslashes($sql) . '");</script>';
+$resultData = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($resultData) > 0) {
+    $allRows = array();
     while ($row = mysqli_fetch_assoc($resultData)) {
+        $allRows[] = $row;
         $jnum = $row['jobID'];
-        $fjobno = substr("000000$jnum",-5);
-/*        echo '<a href="/jdet.php?job_no='.$jnum.'"><div class="jcard" id="'.$jnum.'"><div class="jnum">'.$fjobno.'</div>';
+        $fjobno = substr("000000$jnum", -5);
+        /*        echo '<a href="/jdet.php?job_no='.$jnum.'"><div class="jcard" id="'.$jnum.'"><div class="jnum">'.$fjobno.'</div>';
         echo '<div class="pud">'.date_format(date_create($row['jobDate']),"d-m-Y").'</div>';
         echo '<div class="jcli">'.$row['clientName'].'</div>';
         echo '<div class="jref">'.$row['jobRef'].'</div></div></a>';*/
 
-        echo '<a href="/jdet.php?job_no='.$jnum.'"><div class="jcard" id="'.$jnum.'"><div class="jnum">'.$fjobno.'</div>';
-        echo '<div class="pud">'.date_format(date_create($row['jobDate']),"d-m-Y").'</div>';
-        if (is_null($row['invNum'])) {
-            
+        echo '<a href="/jdet.php?job_no=' . $jnum . '"><div class="jcard" id="' . $jnum . '"><div class="jnum">' . $fjobno . '</div>';
+        if (isset($row['jobDate']) && $row['jobDate'] !== null) {
+            echo '<div class="pud">' . date_format(date_create($row['jobDate']), "d-m-Y") . '</div>';
         } else {
-            echo '<div class="inum">INV: '.$row['invNum'].'</div>';
+            echo '<div class="pud">TBA</div>';
         }
-        echo '<div class="jcli">'.$row['clientName'].'</div>';
-        echo '<div class="jref">'.$row['jobRef'].'</div>';
+        if (is_null($row['invNum'])) {
+        } else {
+            echo '<div class="inum">INV: ' . $row['invNum'] . '</div>';
+        }
+        echo '<div class="jcli">' . $row['clientName'] . '</div>';
+        echo '<div class="jref">' . $row['jobRef'] . '</div>';
 
         echo '</div></a>';
-}
-
+    }
 } else {
-$result = false;
-/*return $result;*/
+    $result = false;
+    /*return $result;*/
 };
-
-
-
-?>
